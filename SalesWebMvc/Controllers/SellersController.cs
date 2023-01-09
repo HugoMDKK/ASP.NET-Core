9383c1809d6e63c7cf797d6bf59550506fedd705
Hugomdkk <victorhugo0856@gmail.com>
2023-01-09 11:51:17 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -40,6 +40,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var ViewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(ViewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -100,6 +106,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var ViewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(ViewModel);
+            }
             if (id != seller.Id)
             {
                 return  RedirectToAction(nameof(Error), new { message = "Id mismatch" });
