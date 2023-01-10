8c573e8ebd4937456ee46cea2fc11fac1cfff937
Hugomdkk <victorhugo0856@gmail.com>
2023-01-10 09:26:24 -0300

    Implementing simple search

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,12 +11,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
 using System.Globalization;
-
+using Microsoft.EntityFrameworkCore;
 
 
 namespace SalesWebMvc
@@ -50,6 +49,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
